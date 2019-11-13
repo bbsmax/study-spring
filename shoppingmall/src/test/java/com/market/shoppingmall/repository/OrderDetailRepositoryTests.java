5db7ae46addcc72fbc5db51cb4cd173c24bc86e2
nataekyun <bbsmax@nate.com>
2019-11-14 04:10:40 +0900

    entity 수정 및 testcode 작성

---
+++ shoppingmall/src/test/java/com/market/shoppingmall/repository/OrderDetailRepositoryTests.java
@@ -0,0 +1,16 @@
+package com.market.shoppingmall.repository;
+
+import com.market.shoppingmall.ShoppingmallApplicationTests;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrderDetailRepositoryTests extends ShoppingmallApplicationTests {
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void create(){
+
+    }
+}
